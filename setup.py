f6e744e3e56d43dc07bafe78183666f900f1a945
Clauter Carlos <clauter.carlos@gmail.com>
2020-03-07 23:54:02 -0300

    Fix package names to permission of pypi

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ def find_package_data(
 
 
 PACKAGE = "libpytools"
-NAME = PACKAGE
+NAME = "pytoolsc2"
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Clauter Carlos"
 AUTHOR_EMAIL = "clauter.carlos@gmail.com"
